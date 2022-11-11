a5ab8aac0ed225c36f2ec052dd3b62512b9e4342
James Vaughan <james@jamesbvaughan.com>
2022-11-11 11:46:35 -0800

    Set up three fiber and text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function JamesLand({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>james.land</title>
+        <meta name="description" content="welcome to james.land" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default JamesLand;
