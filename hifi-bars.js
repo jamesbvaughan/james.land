fd2c9d3229bfbc1ed31b84435d3dcf00a6a960d3
James Vaughan <james@jamesbvaughan.com>
2024-10-16 15:52:22 +0200

    Extract list

--- hifi-bars.js
+++ hifi-bars.js
@@ -1,60 +1,7 @@
-import React from "https://esm.sh/react@18/?dev";
-import ReactDOM from "https://esm.sh/react-dom@18/client?dev";
-import Globe from "https://esm.sh/react-globe.gl@2/?deps=react@18&dev";
+import Globe from "https://esm.sh/globe.gl@^2";
+import hifiBars from "/hifi-bar-list.js";
 
-const hifiBars = [
-  // U.S.
-
-  /// California
-  {
-    name: "Bar Shiru",
-    url: "https://maps.app.goo.gl/3H8hHffbh8hmdoAN6",
-    lat: 37.80641605541192,
-    lng: -122.27070496043847,
-  },
-  {
-    name: "Yokai",
-    url: "https://maps.app.goo.gl/1Had8EYaxNWxC7R19",
-    lat: 37.78934947651318,
-    lng: -122.39843050319782,
-  },
-
-  // Europe
-
-  /// Ireland
-  {
-    name: "Fidelity Bar",
-    url: "https://maps.app.goo.gl/YK6gEFXL4UQsFAmVA",
-    lat: 53.34712096788844,
-    lng: -6.280387338405552,
-  },
-  /// Switzerland
-  {
-    name: "Stereo",
-    url: "https://maps.app.goo.gl/KepcV6LETDmpgJCA7",
-    lat: 47.377943469588175,
-    lng: 8.527304143206587,
-  },
-  /// Spain
-  {
-    name: "Lonely",
-    url: "https://maps.app.goo.gl/7faq9o3Rsyy7FooKA",
-    lat: 41.39884988560985,
-    lng: 2.155175333005087,
-  },
-  {
-    name: "Oblicuo",
-    url: "https://maps.app.goo.gl/R3xbyBb9vNjVquaD8",
-    lat: 41.401476723824,
-    lng: 2.1549146970909336,
-  },
-  {
-    name: "Curtis Audiophile Cafe",
-    url: "https://maps.app.goo.gl/exaHmrbxuoP3hWss9",
-    lat: 41.39210493208592,
-    lng: 2.1578633268503906,
-  },
-];
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 function hifiBarToElement(hifiBar) {
   const element = document.createElement("a");
@@ -71,37 +18,23 @@ function hifiBarToElement(hifiBar) {
   return element;
 }
 
-function App() {
-  const globeRef = React.createRef();
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const [height, setHeight] = React.useState(window.innerHeight);
+const globe = Globe({
+  animateIn: false,
+})(document.getElementById("root"))
+  .globeImageUrl("/images/earth-night.jpg")
+  .backgroundColor(isDarkMode ? "#1e1e2e" : "#EFF1F5")
+  .atmosphereColor(isDarkMode ? "#cdd6f4" : "#4C4F69")
+  .pointOfView({
+    lat: 37.80641605541192,
+    lng: -122.27070496043847,
+    altitude: 1,
+  })
+  .htmlElementsData(hifiBars)
+  .htmlElement(hifiBarToElement)
+  .width(window.innerWidth)
+  .height(window.innerHeight);
 
-  React.useEffect(() => {
-    // globeRef.current.controls().enableDamping = false;
-    globeRef.current.pointOfView({ lat: 37.8, lng: -122, altitude: 1 });
-
-    window.addEventListener("resize", () => {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
-    });
-  }, []);
-
-  return React.createElement(Globe, {
-    ref: globeRef,
-    backgroundColor: "#1e1e2e",
-    animateIn: false,
-    atmosphereColor: "#cdd6f4",
-    pointOfView: {
-      lat: 37.80641605541192,
-      lng: -122.27070496043847,
-    },
-    globeImageUrl: "/images/earth-night.jpg",
-    htmlElementsData: hifiBars,
-    htmlElement: hifiBarToElement,
-    height,
-    width,
-  });
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(React.createElement(App));
+window.addEventListener("resize", () => {
+  globe.width(window.innerWidth);
+  globe.height(window.innerHeight);
+});
