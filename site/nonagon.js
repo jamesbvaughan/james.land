fb099705b95f909e4e11939b90314b7eee49cbcc
James Vaughan <james@jamesbvaughan.com>
2024-11-21 12:03:13 -0800

    Add animation

--- site/nonagon.js
+++ site/nonagon.js
@@ -4,37 +4,22 @@ const ctx = canvas.getContext("2d");
 const numSides = 9;
 const lineThickness = 2;
 
-const dpr = window.devicePixelRatio || 1;
+const vertices = [];
+for (let i = 0; i < numSides; i++) {
+  vertices[i] = { x: 0, y: 0 };
+}
 
-function drawNonagon() {
-  const cssWidth = window.innerWidth;
-  const cssHeight = window.innerHeight;
-
-  canvas.width = cssWidth * dpr;
-  canvas.height = cssHeight * dpr;
-
-  canvas.style.width = `${cssWidth}px`;
-  canvas.style.height = `${cssHeight}px`;
-
-  ctx.scale(dpr, dpr);
-  ctx.clearRect(0, 0, canvas.width / dpr, canvas.height / dpr);
-
-  const centerX = canvas.width / dpr / 2;
-  const centerY = canvas.height / dpr / 2;
-  const radius = Math.min(canvas.width, canvas.height) / dpr / 3;
-
-  const vertices = [];
-  for (let i = 0; i < numSides; i++) {
-    const angle = ((2 * Math.PI) / numSides) * i - Math.PI / 2; // Adjust angle to start at the top
-    const x = centerX + radius * Math.cos(angle);
-    const y = centerY + radius * Math.sin(angle);
-    vertices.push({ x, y });
-  }
-
+function drawNonagon(centerX, centerY, radius, rotation, shadowBlur) {
   ctx.strokeStyle = "red";
   ctx.lineWidth = lineThickness;
   ctx.shadowColor = "red";
-  ctx.shadowBlur = 30 / dpr;
+  ctx.shadowBlur = shadowBlur;
+
+  for (let i = 0; i < numSides; i++) {
+    const angle = ((2 * Math.PI) / numSides) * i - Math.PI / 2 + rotation;
+    vertices[i].x = centerX + radius * Math.cos(angle);
+    vertices[i].y = centerY + radius * Math.sin(angle);
+  }
 
   for (let i = 0; i < numSides; i++) {
     for (let j = i + 1; j < numSides; j++) {
@@ -54,5 +39,39 @@ function drawNonagon() {
   ctx.stroke();
 }
 
-window.addEventListener("resize", drawNonagon);
-drawNonagon();
+let rotation = 0;
+
+let cssWidth, cssHeight, dpr;
+function resizeCanvas() {
+  dpr = window.devicePixelRatio || 1;
+
+  cssWidth = window.innerWidth;
+  cssHeight = window.innerHeight;
+
+  canvas.width = cssWidth * dpr;
+  canvas.height = cssHeight * dpr;
+
+  canvas.style.width = `${cssWidth}px`;
+  canvas.style.height = `${cssHeight}px`;
+
+  ctx.scale(dpr, dpr);
+}
+
+window.addEventListener("resize", resizeCanvas);
+
+function animate(timestamp) {
+  ctx.clearRect(0, 0, canvas.width / dpr, canvas.height / dpr);
+
+  const centerX = canvas.width / dpr / 2;
+  const centerY = canvas.height / dpr / 2;
+
+  const size = Math.min(cssWidth, cssHeight) / 2 - 24;
+  rotation += 0.0001;
+  const shadowBlur = Math.floor((Math.sin(timestamp / 1000) + 1) * 48);
+  drawNonagon(centerX, centerY, size, rotation, shadowBlur);
+
+  requestAnimationFrame(animate);
+}
+
+resizeCanvas();
+animate();
